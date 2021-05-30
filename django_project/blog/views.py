d6b3300fb66c49fbeac68bf1d67c3fc0dbc8eaca
Zuzanna Zajkiewicz <machefi1@gmail.com>
2021-05-30 16:03:16 +0200

    Add posts model

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,9 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 29, 2018'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'title': 'homepage'
     }
     return render(request, 'blog/home.html', context)
